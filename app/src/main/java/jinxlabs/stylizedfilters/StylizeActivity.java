3feb0f6055122aa8c06df160b985c0e0998eed2e
Udit Mukherjee <uditmukherjee457@gmail.com>
2017-07-14 16:56:57 +0530

    Pass the image taken from the camera activity to the stylize activity.

--- app/src/main/java/jinxlabs/stylizedfilters/StylizeActivity.java
+++ app/src/main/java/jinxlabs/stylizedfilters/StylizeActivity.java
@@ -1,13 +1,29 @@
 package jinxlabs.stylizedfilters;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.File;
 
 public class StylizeActivity extends AppCompatActivity {
+
+    private ImageView imagePreview;
+    private File imageFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stylize);
+
+        String filePath = getIntent().getExtras().getString("IMAGE_FILE_URL");
+
+        imageFile = new File(filePath);
+
+        Bitmap imageBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+        imagePreview = (ImageView) findViewById(R.id.filter_preview);
+        imagePreview.setImageBitmap(imageBitmap);
     }
 }
